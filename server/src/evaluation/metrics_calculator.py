4d739feac4209d4b82363e2c94a9168c397f4c59
Lily Martin <lilyhmar@amazon.com>
2025-12-10 14:10:18 +1100

    added pause to metrics calculator as well

--- server/src/evaluation/metrics_calculator.py
+++ server/src/evaluation/metrics_calculator.py
@@ -139,7 +139,7 @@ Evaluate the actual response."""
         
         evaluations = []
         
-        for question in self.dataset['questions']:
+        for i, question in enumerate(self.dataset['questions']):
             question_id = question['id']
             
             # Find corresponding result
@@ -162,7 +162,11 @@ Evaluate the actual response."""
             )
             
             evaluations.append(eval_result)
-        
+        # Add pause between evaluations to avoid rate limiting
+        if i < len(self.dataset['questions']) - 1:  # Don't pause after last item
+            logger.info("Pausing 2 seconds to avoid rate limiting...")
+            await asyncio.sleep(2)      
+  
         # Calculate summary statistics
         summary = self._calculate_summary(evaluations)
         
